924443a240199b1ec6504f627c6e49fb171c445e
Ivan583 <ivanych2293@gmail.com>
2020-10-26 14:39:55 +0300

    Создание компонента списка

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoList from "./Todo/TodoList";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>React Learn project </h1>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Todo/TodoList.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function TodoList() {
+  return (
+    <ul>
+      <li>Point 1</li>
+      <li>Point 2</li>
+    </ul>
+  );
+}
