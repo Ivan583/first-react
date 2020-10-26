bea3df4a1f5440fb58f3043388c992f853e55817
Ivan583 <ivanych2293@gmail.com>
2020-10-26 13:19:51 +0300

    Создание заголовка

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 // import React from 'react';
 
 function App() {
-  return <div className="wrapper" />;
+  return (
+    <div className="wrapper">
+      <h1>React Learn project</h1>
+    </div>
+  );
 }
 
 export default App;
