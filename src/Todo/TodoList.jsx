9af82d0d2e793641351de9d8a3592a52088210b0
Ivan583 <ivanych2293@gmail.com>
2020-10-26 15:11:41 +0300

    Стили списка

--- src/Todo/TodoList.jsx
+++ src/Todo/TodoList.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 
+const styles = {
+  ul: {
+    listStyle: "none",
+    margin: 0,
+    padding: 0
+  }
+};
+
 export default function TodoList() {
   return (
-    <ul>
+    <ul style={styles.ul}>
       <li>Point 1</li>
       <li>Point 2</li>
     </ul>
