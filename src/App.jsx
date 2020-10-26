df72c0bb3da56b14efe53e4f7dd427fefe331d4c
Ivan583 <ivanych2293@gmail.com>
2020-10-26 15:38:56 +0300

    Передача свойств в компонент TodoList

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import TodoList from "./Todo/TodoList";
 
 function App() {
+  let todos = [
+    { id: 1, completed: false, title: "Купить хлеб" },
+    { id: 2, completed: false, title: "Накормить кота" },
+    { id: 3, completed: false, title: "Закончить проект" }
+  ];
+
   return (
     <div className="wrapper">
       <h1>React Learn project </h1>
-      <TodoList />
+      <TodoList tasks={todos} />
     </div>
   );
 }
