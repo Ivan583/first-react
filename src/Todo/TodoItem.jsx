48108afef96580ab18cae837dbb0c834aae0795d
Ivan583 <ivanych2293@gmail.com>
2020-10-26 22:08:30 +0300

    Стили элементов списка

--- src/Todo/TodoItem.jsx
+++ src/Todo/TodoItem.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+const styles = {
+  li: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '0.5rem 1rem',
+    border: '1px solid green',
+    borderRadius: '4px',
+    marginBottom: '0.5rem'
+  },
+  input: {
+    marginRight: '1rem'
+  }
+}
+
 function TodoItem({ task, index }) {
   return (
-    <li>
-      <strong>{index + 1}</strong>
-      {task.title}
+    <li style={styles.li}>
+      <span>
+        <input type="checkbox" style={styles.input} />
+        <strong>{index + 1}</strong>
+        &nbsp;
+        {task.title}
+      </span>
+      <button className="rm">&times;</button>
     </li>
   );
 }
