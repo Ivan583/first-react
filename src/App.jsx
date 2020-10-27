a623df6b49e0df327ca4b7121eaffceeaf72db8d
Ivan583 <ivanych2293@gmail.com>
2020-10-27 15:59:49 +0300

    Animation

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import React, { useEffect } from "react";
 import TodoList from "./Todo/TodoList";
 import Context from './context';
 import AddTodo from "./Todo/AddTodo";
+import Loader from "./Loader";
 
 function App() {
   const [todos, setTodos] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
       .then(response => response.json())
       .then(todos => {
         setTimeout(() => {
-          setTodos(todos)
+          setTodos(todos);
+          setLoading(false);
         }, 2000)
       })
   }, [])
@@ -44,7 +47,9 @@ function App() {
       <div className="wrapper">
         <h1>React Learn project </h1>
         <AddTodo onCreate={addTodo} />
-        {todos.length ? <TodoList tasks={todos} onToggle={toggleTask} /> : <p>No tasks.</p>}
+        {loading && <Loader />}
+        {todos.length ? (<TodoList tasks={todos} onToggle={toggleTask} />
+        ) : loading ? null : (<p>No tasks.</p>)}
       </div>
     </Context.Provider>
   );
