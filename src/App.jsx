ae3872e82bdf6f369766674cbeb35d908d87929e
Ivan583 <ivanych2293@gmail.com>
2020-10-27 12:54:55 +0300

    Добавление элемента списка

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TodoList from "./Todo/TodoList";
 import Context from './context';
+import AddTodo from "./Todo/AddTodo";
 
 function App() {
   const [todos, setTodos] = React.useState([
@@ -24,10 +25,19 @@ function App() {
     setTodos(todos.filter(todo => todo.id !== id));
   }
 
+  function addTodo(title) {
+    setTodos(todos.concat([{
+      title,
+      id: Date.now(),
+      completed: false
+    }]))
+  }
+
   return (
     <Context.Provider value={{ removeTask }}>
       <div className="wrapper">
         <h1>React Learn project </h1>
+        <AddTodo onCreate={addTodo} />
         {todos.length ? <TodoList tasks={todos} onToggle={toggleTask} /> : <p>No tasks.</p>}
       </div>
     </Context.Provider>
