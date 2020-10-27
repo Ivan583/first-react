f10c441aba858392ccd3e41d05425d855745cc0f
Ivan583 <ivanych2293@gmail.com>
2020-10-27 23:19:37 +0300

    Изменение анимации

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 
 export default () => (
     <div style={{ display: 'flex', justifyContent: 'center', margin: '0.5rem' }}>
-        <div className="lds-dual-ring" /></div>
+        <div className="lds-heart"><div /></div></div>
 ) 
